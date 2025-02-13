5dd9b4b8736e873e25d99cee832de39b9bc9de00
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-02-13 18:21:17 +0300

    m

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,12 +23,6 @@ export default defineConfig({
   server: {
     // Exposes your dev server and makes it accessible for the devices in the same network.
     host: true,
-  },
-
-  resolve: {
-    alias: {
-      '@telegram-apps/sdk': resolve('node_modules/@telegram-apps/sdk/src'),
-    },
   }
 });
 
