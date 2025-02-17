36a24e061396d3a025de241b43d5c61faf30146c
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-02-17 15:47:02 +0300

    m

--- src/components/DateTimePicker.tsx
+++ src/components/DateTimePicker.tsx
@@ -1,6 +1,8 @@
+import { useTranslation } from "react-i18next";
 import Wheel from "./wheel"
 
 export default () => {
+    const { t } = useTranslation();
 
     const currentYear = new Date().getFullYear() - 18
     const years = Array.from({ length: 100 }, (_, i) => currentYear - i)
@@ -19,18 +21,20 @@ export default () => {
       }}
     >
       <div style={{ width: "40%", height: 180 }}>
+        <p className="text-center text-black-500">{t("year")}</p>
         <Wheel
-          loop
           length={years.length}
           width={140}
           setValue={formatYear}
         />
       </div>
       <div style={{ width: "30%", height: 180 }}>
-        <Wheel loop length={12} width={23} />
+        <p className="text-center text-black-500">{t("month")}</p>
+        <Wheel length={12} width={23} />
       </div>
       <div style={{ width: "30%", height: 180 }}>
-        <Wheel loop length={12} width={23} />
+        <p className="text-center text-black-500">{t("day")}</p>
+        <Wheel length={12} width={23} />
       </div>
     </div>
   )
