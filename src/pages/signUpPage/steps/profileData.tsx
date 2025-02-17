0459e4e7cc920db17f8a3e0c35df8e2ed84d9f0a
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-02-17 16:13:36 +0300

    n

--- src/pages/signUpPage/steps/profileData.tsx
+++ src/pages/signUpPage/steps/profileData.tsx
@@ -27,18 +27,20 @@ const ProfileDataStep = () => {
 
         </Section>
 
-        <div
-                className="grid gap-2 grid-cols-2 py-2"
-
+        <Section
+                header={t("Iam")}
             >
-                <Chip mode="elevated" Component="label" before={<Radio name="Male" defaultChecked />}>
-                    {t("Male")}
-                </Chip>
+                <div className="grid gap-2 grid-cols-2 py-2">
+                    <Chip mode="elevated" Component="label" before={<Radio name="Male" defaultChecked />}>
+                        {t("Male")}
+                    </Chip>
 
-                <Chip mode="elevated" Component="label" before={<Radio name="Female" />}>
-                    {t("Female")}
-                </Chip>
-        </div>
+                    <Chip mode="elevated" Component="label" before={<Radio name="Female" />}>
+                        {t("Female")}
+                    </Chip>
+                </div>
+
+        </Section>
 
         <Section header={t("Selectdateofbirth")}>
             <DatePicker/>
